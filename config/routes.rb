b4d84bd3630819274079c8f1fb4f9ff09531eb5d
近藤ちひろ <kondochihiro@kondouchihironoMacBook-Air.local>
2026-07-03 17:33:02 +0900

    ヘッダーロゴを画像に変更

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :users
+  resource :roooms
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_07_02_113123) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_03_051057) do
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +36,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_07_02_113123) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "rooms", "users"
 end
