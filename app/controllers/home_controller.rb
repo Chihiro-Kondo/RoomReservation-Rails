aa9e4a9cfb76393c39272bb6c30c3e773ca84b74
近藤ちひろ <kondochihiro@kondouchihironoMacBook-Air.local>
2026-07-05 23:27:26 +0900

    mainの検索欄を仮で作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,9 @@
 class HomeController < ApplicationController
-
+  def index
+    if params[:keyword].present?
+      @rooms = Room.where("address LIKE ?", "%#{params[:keyword]}%")
+    else 
+      @rooms = Room.all
+    end
+  end
 end
